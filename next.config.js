e1146a42897a79d93315c536c8a803012ef63844
Adam Kimmerer <adamkimmerer@gmail.com>
2025-10-18 18:04:19 -0400

    Removed next experimental features

--- next.config.js
+++ next.config.js
@@ -6,10 +6,6 @@ const nextConfig = {
     // Enables the styled-components SWC transform
     styledComponents: true
   },
-  experimental:{
-    //Only For Next.js versions prior to 14.1.0 because it is enabled by default since version 14.1.0 
-    windowHistorySupport:true 
-},
 }
 
 module.exports = nextConfig
